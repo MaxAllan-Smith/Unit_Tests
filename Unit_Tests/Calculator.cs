f9ddaa7062933a255e8506b33778b60eee651d6c
Max Allan-Smith <Maxibon_1195@hotmail.com>
2024-06-23 21:22:58 +0100

    Unit Tests Initialized

---
+++ Unit_Tests/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Unit_Tests
+{
+	public class Calculator
+	{
+		public Calculator()
+		{
+		}
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
+
---
+++ Unit_Tests_Test/CalculatorTests.cs
@@ -0,0 +1,39 @@
+﻿using Unit_Tests;
+using Xunit;
+
+namespace Unit_Tests_Test
+{
+	public class CalculatorTests
+	{
+		[Fact]
+		public void Add_TwoNumbers_ReturnsSum()
+		{
+			// Arrange
+			var calculator = new Calculator();
+			int num1 = 2;
+			int num2 = 2;
+
+			// Act
+			int result = calculator.Add(num1, num2);
+
+			// Assert
+			Assert.Equal(4, result);
+		}
+
+		[Fact]
+		public void Subtract_TwoNumbers_ReturnSum()
+		{
+			// Arrange
+			var calculator = new Calculator();
+			int num1 = 4;
+			int num2 = 2;
+
+			// Act
+			int result = calculator.Subtract(num1, num2);
+
+			// Assert
+			Assert.Equal(2, result);
+		}
+	}
+}
+
